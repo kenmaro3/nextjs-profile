2d2c48c7ad5d1d028d089e28c67f4e17eb25b88b
kenmaro <35465898+kenmaro3@users.noreply.github.com>
2023-09-21 13:23:13 +0900

    Update index.tsx

--- src/components/cases-section/index.tsx
+++ src/components/cases-section/index.tsx
@@ -103,9 +103,7 @@ const articles = [
 const CasesSection = () => (
   <section>
     <div className={tw(`w-full min-h-screen bg-gray-900 relative`)}>
-      <div className={tw(`absolute left-0 top-0 h-screen w-full overflow-hidden`)}>
-{/*         <ParticleBg /> */}
-      </div>
+      <div className={tw(`absolute left-0 top-0 h-screen w-full overflow-hidden`)}>{/* <ParticleBg /> */}</div>
       <div className={tw(`max-w-7xl mx-4 lg:mx-auto pt-20 lg:pt-40`)}>
         {/* <h1 className={tw(`text-white text-4xl lg:text-7xl font-bold text-center`)}>What will you build?</h1> */}
         <p className={tw(`lg:text-2xl text-white text-gray-400 text-center text-xl mt-12`)}>個人開発プロジェクト</p>
