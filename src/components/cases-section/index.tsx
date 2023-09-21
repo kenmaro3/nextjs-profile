382f5fd2dc5b0e50bceb6d54c1ea7d3f0dfdb1f7
kenmaro <35465898+kenmaro3@users.noreply.github.com>
2023-09-21 12:55:42 +0900

    Update index.tsx

--- src/components/cases-section/index.tsx
+++ src/components/cases-section/index.tsx
@@ -1,47 +1,46 @@
 import { tw } from 'twind';
-import Particles from 'react-particles';
 import Arrow from '@/constants/svg/arrow.svg';
 
-const ParticleBg = () => (
-  <Particles
-    params={{
-      particles: {
-        number: {
-          value: 400,
-          density: {
-            enable: true,
-            value_area: 3000,
-          },
-        },
-        line_linked: {
-          enable: false,
-        },
-        move: {
-          direction: `right`,
-          speed: 0.3,
-        },
-        size: {
-          value: 1,
-        },
-        opacity: {
-          anim: {
-            enable: true,
-            speed: 0.5,
-            opacity_min: 0.1,
-          },
-        },
-      },
-      interactivity: {
-        events: {
-          onclick: {
-            enable: false,
-          },
-        },
-      },
-      retina_detect: true,
-    }}
-  />
-);
+// const ParticleBg = () => (
+//   <Particles
+//     params={{
+//       particles: {
+//         number: {
+//           value: 400,
+//           density: {
+//             enable: true,
+//             value_area: 3000,
+//           },
+//         },
+//         line_linked: {
+//           enable: false,
+//         },
+//         move: {
+//           direction: `right`,
+//           speed: 0.3,
+//         },
+//         size: {
+//           value: 1,
+//         },
+//         opacity: {
+//           anim: {
+//             enable: true,
+//             speed: 0.5,
+//             opacity_min: 0.1,
+//           },
+//         },
+//       },
+//       interactivity: {
+//         events: {
+//           onclick: {
+//             enable: false,
+//           },
+//         },
+//       },
+//       retina_detect: true,
+//     }}
+//   />
+// );
 
 const articles = [
   {
@@ -105,7 +104,7 @@ const CasesSection = () => (
   <section>
     <div className={tw(`w-full min-h-screen bg-gray-900 relative`)}>
       <div className={tw(`absolute left-0 top-0 h-screen w-full overflow-hidden`)}>
-        <ParticleBg />
+{/*         <ParticleBg /> */}
       </div>
       <div className={tw(`max-w-7xl mx-4 lg:mx-auto pt-20 lg:pt-40`)}>
         {/* <h1 className={tw(`text-white text-4xl lg:text-7xl font-bold text-center`)}>What will you build?</h1> */}
